4d30e2e28426b2a60adc4c601be5b7844dddff87
Henrique Silva <jhenrique09.mcz@hotmail.com>
2022-07-26 04:34:13 +0800

    sm8150-common: releasetools: Fix Incremental OTA Gen
    Change-Id: Iea0cc9cafe72ffd33c7617447bd2765251fc5e75
    Signed-off-by: Zinadin Zidan <tuhinsultana075@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -18,25 +18,29 @@ import common
 import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, False)
   return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, True)
   return
 
-def AddImage(info, basename, dest):
+def AddImage(info, basename, dest, incremental):
   path = "IMAGES/" + basename
-  if path not in info.input_zip.namelist():
+  if incremental:
+    input_zip = info.source_zip
+  else:
+    input_zip = info.input_zip
+  if path not in input_zip.namelist():
     return
 
-  data = info.input_zip.read(path)
+  data = input_zip.read(path)
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def OTA_InstallEnd(info):
+def OTA_InstallEnd(info, incremental):
   info.script.Print("Patching dtbo and vbmeta images...")
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
-  AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo", incremental)
+  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta", incremental)
+  AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system", incremental)
   return
